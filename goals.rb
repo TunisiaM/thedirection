21ccdda755a60fcbed4bde9b4da15016445e0af1
Projinks User <tunisiacmitchell@gmail.com>
2015-05-05 20:48:42 -0400

    My first commit

---
+++ goals.rb
@@ -0,0 +1,4 @@
+# Tunisia Mitchell
+# Goal: Learn the basics of a back-end language. Therefore, on my own, can practice and continue learning... bringing me one step closer to becomming a full stack developer.
+# Goal: Get comfortable with programming jargon.
+# Goal: Surround myself with other people who wish to become developers.
